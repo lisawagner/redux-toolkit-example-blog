7acfccbfe8be0f5b98f1d663214cf0c25c0e6236
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-19 15:57:35 -0700

    fix: rejected fetchPosts now working as expected

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -113,17 +113,21 @@ const postsSlice = createSlice({
       state.posts = state.posts.concat(loadedPosts)
     })
     .addCase(fetchPosts.rejected, (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+  })
+    // .addCase(addNewPost.fulfilled, (state, action) => {
       // FIX: This is a 'fix' for the API postIDs
       // Creating sortedPosts & assigning the id would not be needed
       // if the fake API returned accurate new post IDs
-      const sortedPosts = state.posts.sort((a, b) => {
-        if(a.id > b.id) return 1
-        if(a.id < b.id) return -1
-        return 0
-      })
-      action.payload.id = sortedPosts[sortedPosts.length -1].id + 1
+      // const sortedPosts = state.posts.sort((a, b) => {
+      //   if(a.id > b.id) return 1
+      //   if(a.id < b.id) return -1
+      //   return 0
+      // })
+      // action.payload.id = sortedPosts[sortedPosts.length -1].id + 1
       // end FIX
-    })
+    // })
   }
 })
 
