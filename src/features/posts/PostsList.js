ae871d066208f3f0d435c9170113d9880c4130bc
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-19 15:36:10 -0700

    refactor: mod code to consume JSON placeholder API

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,11 +1,22 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postsSlice";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from './TimeAgo'
 import ReactionButtons from "./ReactionButtons";
 
 const PostsList = () => {
+  const dispatch = useDispatch()
+
   const posts = useSelector(selectAllPosts)
+  const postsStatus = useSelector(getPostsStatus)
+  const error = useSelector(getPostsError)
+
+  useEffect(() => {
+    if(postsStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [postsStatus, dispatch])
 
   const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,34 +1,54 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
+import axios from 'axios'
 
-const initialState = [
-  {
-    id: '1',
-    title: 'Learning Redux Toolkit',
-    content: "I've heard good things about redux-toolkit",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    }
-  },
-  {
-    id: '2',
-    title: 'Slices...',
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0
-    }
+// define base URL - using JSONPlaceholder free REST API for test data.
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+// const initialState = [
+//   {
+//     id: '1',
+//     title: 'Learning Redux Toolkit',
+//     content: "I've heard good things about redux-toolkit",
+//     date: sub(new Date(), { minutes: 10 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0
+//     }
+//   },
+//   {
+//     id: '2',
+//     title: 'Slices...',
+//     content: "The more I say slice, the more I want pizza.",
+//     date: sub(new Date(), { minutes: 5 }).toISOString(),
+//     reactions: {
+//       thumbsUp: 0,
+//       wow: 0,
+//       heart: 0,
+//       rocket: 0,
+//       coffee: 0
+//     }
+//   }
+// ]
+
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+// add async thunk to get posts
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  try {
+    const response = await axios.get(POSTS_URL)
+    return [...response.data]
+  } catch (err) {
+    return err.message
   }
-]
+})
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -37,7 +57,7 @@ const postsSlice = createSlice({
     addPost: {
       reducer(state, action) {
         // the payload is the form data we send /dispatch (action)
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare(title, content, userId) {
         return {
@@ -61,15 +81,56 @@ const postsSlice = createSlice({
     // track the reactions count state. Immer handles the state mutation
     addReaction(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
       if(existingPost) {
         existingPost.reactions[reaction]++
       }
     }
+  },
+  // builder parameter is an object that lets us define additional case reducers
+  // that run in response to the actions defined outside of the slice
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // add date and reactions
+        let min = 1
+        const loadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString()
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0
+        }
+        return post
+      })
+      // add posts tot the array
+      state.posts = state.posts.concat(loadedPosts)
+    })
+    .addCase(fetchPosts.rejected, (state, action) => {
+      // FIX: This is a 'fix' for the API postIDs
+      // Creating sortedPosts & assigning the id would not be needed
+      // if the fake API returned accurate new post IDs
+      const sortedPosts = state.posts.sort((a, b) => {
+        if(a.id > b.id) return 1
+        if(a.id < b.id) return -1
+        return 0
+      })
+      action.payload.id = sortedPosts[sortedPosts.length -1].id + 1
+      // end FIX
+    })
   }
 })
 
-export const selectAllPosts = (state) => state.posts
+export const selectAllPosts = (state) => state.posts.posts
+export const getPostsStatus = (state) => state.posts.status
+export const getPostsError = (state) => state.posts.error
+
 // export that actions creator function automattically created by rtk
 export const { addPost, addReaction } = postsSlice.actions
 
