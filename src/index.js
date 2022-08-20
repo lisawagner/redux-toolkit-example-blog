f07d50bfce258679653ed4bf4f20228e0420c67d
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-20 10:41:54 -0700

    feat: add dispatch fetchUsers at app load time

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { fetchUsers } from './features/users/usersSlice';
 import App from './App';
 import './index.css';
+
+// fetch users(authors) immediately when app loads
+store.dispatch(fetchUsers())
 
 const container = document.getElementById('root');
 const root = createRoot(container);
