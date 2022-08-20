e8c1f6dc28f2709edee04fb7091c5925d8765980
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-20 10:40:11 -0700

    feat: add fetchUsers async thunk

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: '0', name: 'Tanya Prochazka' },
-  { id: '1', name: 'Madam Zira' },
-  { id: '2', name: 'Makenzie Davis' },
-]
+// define the users(authors) URL
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+// const initialState = [
+//   { id: '0', name: 'Tanya Prochazka' },
+//   { id: '1', name: 'Madam Zira' },
+//   { id: '2', name: 'Makenzie Davis' },
+// ]
+
+const initialState = []
+
+// get users from the REST API
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    const response = await axios.get(USERS_URL)
+    // you can return just response.data instead of a new array
+    return [...response.data]
+  } catch (error) {
+    return error.message
+  }
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // Here the state is completely overwritten, so 'users' are not accidentally
+      // added twice vs using state.push to spread the payload into the array
+      return action.payload
+    })
+  }
+
 })
 
 export const selectAllUsers = (state) => state.users;
