3fe3f1d4e1dc3df32a600409b19feac190f83d69
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-21 09:27:01 -0700

    chore: clean up code comments

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -5,35 +5,6 @@ import axios from 'axios'
 // define base URL - using JSONPlaceholder free REST API for test data.
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
-// const initialState = [
-//   {
-//     id: '1',
-//     title: 'Learning Redux Toolkit',
-//     content: "I've heard good things about redux-toolkit",
-//     date: sub(new Date(), { minutes: 10 }).toISOString(),
-//     reactions: {
-//       thumbsUp: 0,
-//       wow: 0,
-//       heart: 0,
-//       rocket: 0,
-//       coffee: 0
-//     }
-//   },
-//   {
-//     id: '2',
-//     title: 'Slices...',
-//     content: "The more I say slice, the more I want pizza.",
-//     date: sub(new Date(), { minutes: 5 }).toISOString(),
-//     reactions: {
-//       thumbsUp: 0,
-//       wow: 0,
-//       heart: 0,
-//       rocket: 0,
-//       coffee: 0
-//     }
-//   }
-// ]
-
 const initialState = {
   posts: [],
   status: 'idle',
