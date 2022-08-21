57cc8b5b837d11dbed1d7a393163fdee00a9dace
Lisa Wagner <lisawagner.online@gmail.com>
2022-08-21 11:06:26 -0700

    feat: add React Router to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+// redux
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { fetchUsers } from './features/users/usersSlice';
-import App from './App';
+// styles
 import './index.css';
 
 // fetch users(authors) immediately when app loads
@@ -19,7 +22,11 @@ root.render(
   //    Don't use strictmode, or downgrade to react 17.
   // <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   // </React.StrictMode>
 );
